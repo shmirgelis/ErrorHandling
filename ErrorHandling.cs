ba0d3a0ecfc0fc1436db2342cdb23b357a574e1f
Audrius Budrys <budrys@email.com>
2021-01-10 22:25:19 +0200

    Add project files.

---
+++ ErrorHandling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        try
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+        catch (NotImplementedException)
+        {
+
+            throw new Exception("Exception Handled by throwing new exception");
+        }
+       
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
